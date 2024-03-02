f22cd45e8465139f0e6b2dc5596c6de51d0349df
Farasat Ali <farasataliazeemi1@gmail.com>
2024-03-02 15:20:12 +0500

    upd: add pwa capability

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// @ts-check
+import withSerwistInit from "@serwist/next";
 
-export default nextConfig;
+const withSerwist = withSerwistInit({
+  cacheOnFrontEndNav: true,
+  swSrc: "src/sw.ts", // add the path where you create sw.ts
+  swDest: "public/sw.js",
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === "development", // to disable pwa in development
+  // ... other options
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  swcMinify: true,
+  reactStrictMode: true,
+  // ... other next.js config options
+};
+
+export default withSerwist(nextConfig);
